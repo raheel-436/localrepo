cd831838622614ba910994cf92b004f84da58096
Raheel Ahmed <raheelkk436@gmail.com>
2023-09-01 19:18:04 +0500

    add new feature

--- main.sol
+++ main.sol
@@ -1,8 +1,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 contract A {
-    uint public val;
+    uint public val1;
+    uint public val2;
     constructor() {
-        val = 10;
+        val1 = 10;
+        val2 = 20;
     }
 }
